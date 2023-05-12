b3c8327c454319e4e46e0e0f178904c75f9e66c4
Leo Antunes <antunesleo4@gmail.com>
2023-05-12 13:55:18 -0300

    add docker-compose file

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Note(models.Model):
+   id = models.AutoField(primary_key=True)
+   title = models.CharField(max_length=255)
+   content = models.TextField()
+   created_at = models.DateTimeField(default=timezone.now)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self):
+       return self.title
+
+   def as_dict(self):
+       return {
+           "id": self.id,
+           "title": str(self.title),
+           "content": str(self.content),
+           "created_at": datetime.strftime(self.created_at, "%Y-%m-%d %H:%M:%S"),
+           "updated_at": datetime.strftime(self.updated_at, "%Y-%m-%d %H:%M:%S"),
+       }
